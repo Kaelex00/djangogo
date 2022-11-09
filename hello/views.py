aa3de2db37413088ef9850a6994770714457ebe4
evejaira <evejaiagustin@gmail.com>
2022-11-09 15:46:21 +0800

    initial "commit"

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("Hello, BSIT 4C!")
+
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, BSIT 4B!")
+# Create your views here.
+
